6c0d9a48112be912795df91fb8e38b09ccad0173
donjins <josephdon.dev@gmail.com>
2025-07-02 13:53:46 +0530

    studing loop

---
+++ day2/loops.py
@@ -0,0 +1,12 @@
+age = 18
+if age > 18:
+  print("lisence got")
+elif age == 18:
+  print("eligible")
+else:
+  print("no lisence")
+
+a = 15
+if a > 10:
+  if a < 20:
+    print("number is between 10 and 20")
