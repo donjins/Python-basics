5db82b3e0d8597d419fed6c8ba744cd7b821b72d
donjins <josephdon.dev@gmail.com>
2025-07-02 15:49:03 +0530

    studing loops

--- day2/loops.py
+++ day2/loops.py
@@ -10,3 +10,22 @@ a = 15
 if a > 10:
   if a < 20:
     print("number is between 10 and 20")
+
+user = int(input("enter the number?"))
+if user > 0:
+  print("Positive")
+elif user < 0:
+  print("negative")
+else:
+  print("Zero")
+
+
+user1 = int(input("enter the number between 1 -100?"))
+if user1 > 90:
+  print("A")
+elif user1 > 75 & user1 < 89:
+  print("B")
+elif user1 > 60&user1 < 74:
+  print("")
+else:
+  print("negative")
