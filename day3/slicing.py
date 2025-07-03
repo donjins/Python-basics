49601eab45c2d66e70cb38af8ebdad3140ab4621
donjins <josephdon.dev@gmail.com>
2025-07-03 21:23:30 +0530

    studing slicing

---
+++ day3/slicing.py
@@ -0,0 +1,6 @@
+numbers = [1, 2, 3, 4, 5]
+print(numbers[1:4])    # [2, 3, 4]
+print(numbers[:3])     # [1, 2, 3]
+print(numbers[::2])    # [1, 3, 5]
+print(numbers[::-1])   # reversed list: [5,4,3,2,1]
+
