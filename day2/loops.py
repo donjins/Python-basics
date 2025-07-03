16daca4581e67fcc0af67a203ef15174637573ba
donjins <josephdon.dev@gmail.com>
2025-07-03 21:00:04 +0530

    studing loops

--- day2/loops.py
+++ day2/loops.py
@@ -1,31 +1,56 @@
-age = 18
-if age > 18:
-  print("lisence got")
-elif age == 18:
-  print("eligible")
+if 7 % 2 == 0:
+  print("even")
 else:
-  print("no lisence")
+  print("Odd")
 
-a = 15
-if a > 10:
-  if a < 20:
-    print("number is between 10 and 20")
 
-user = int(input("enter the number?"))
-if user > 0:
-  print("Positive")
-elif user < 0:
-  print("negative")
+# numb = [10, 25 ,15 ]
+# b = numb[0]
+# for num in numb:
+#  if numb[num] > b:
+#   print(b)
+
+
+quest = int(input(" enter the year"))
+
+if quest % 400 == 0:
+  print("leap year")
+elif quest % 100 == 0:
+  print("Leap year")
+elif quest % 4 ==0:
+  print("leap year")
 else:
-  print("Zero")
+  print("not leep year")
 
 
-user1 = int(input("enter the number between 1 -100?"))
-if user1 > 90:
-  print("A")
-elif user1 > 75 & user1 < 89:
-  print("B")
-elif user1 > 60&user1 < 74:
-  print("")
-else:
-  print("negative")
+secretnumber = 6
+chance = 0
+while chance < 3:
+  val = int(input("enter the number?"))
+  print(val)
+  if secretnumber == val:
+    print("Sucess")
+    break
+  else:
+    print("try again")
+  chance += 1
+
+
+passw = "admin123"
+i = 0
+while 1 < 5:
+  valu = input(" enter the password")
+  if passw == valu:
+
+    print("login successfull")
+    break
+  else:
+    print("try again")
+  i += 1
+print("chance is over")
+
+
+for x in range(20):
+  if x % 3 == 0:
+    continue
+  print(x)
