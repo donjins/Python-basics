b5758875fda9699323ee575c47f65ab97ae6b908
donjins <josephdon.dev@gmail.com>
2025-07-04 23:57:09 +0530

    studing recursion factorail

--- day4/functions.py
+++ day4/functions.py
@@ -68,3 +68,11 @@ def reverse(s):
 
 print(reverse("hello"))
 
+def is_palindrome(s):
+    if len(s) <= 1:
+        return True
+    if s[0] != s[-1]:
+        return False
+    return is_palindrome(s[1:-1])
+
+print(is_palindrome("madam"))
