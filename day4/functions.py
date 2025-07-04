aaea3dc9e8c0807e49d45ea770b73fc0b253669b
donjins <josephdon.dev@gmail.com>
2025-07-04 23:57:29 +0530

    studing recursion factorail

--- day4/functions.py
+++ day4/functions.py
@@ -76,3 +76,10 @@ def is_palindrome(s):
     return is_palindrome(s[1:-1])
 
 print(is_palindrome("madam"))
+
+def sum_of_digits(n):
+    if n == 0:
+        return 0
+    return (n % 10) + sum_of_digits(n // 10)
+
+print(sum_of_digits(1234))
